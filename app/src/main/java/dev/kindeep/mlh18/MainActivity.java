ccf84d2fea450c5859fbefeac4ac76db0271f5eb
Kindeep <kindeepsingh@gmail.com>
2018-12-01 16:39:17 -0500

    no crash on numbers.

--- app/src/main/java/dev/kindeep/mlh18/MainActivity.java
+++ app/src/main/java/dev/kindeep/mlh18/MainActivity.java
@@ -3,13 +3,12 @@ package dev.kindeep.mlh18;
 import android.os.StrictMode;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,19 +17,18 @@ import com.microsoft.cognitiveservices.speech.SpeechConfig;
 import com.microsoft.cognitiveservices.speech.SpeechRecognitionResult;
 import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
 
-import java.io.Console;
-import java.util.ArrayList;
 import java.util.Locale;
 import java.util.concurrent.Future;
 
 import static android.Manifest.permission.*;
 
 public class MainActivity extends AppCompatActivity {
-//i changes
+    //i changes
     TextToSpeech t1;
     TextView ed1;
     Button b1;
-
+    Button hear;
+    boolean recognized;
     SpeechRecognitionResult result;
     // Replace below with your own subscription key
     private static String speechSubscriptionKey = "818b0c8310c942f5b2a4c20e769a3e74";
@@ -41,18 +39,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (android.os.Build.VERSION.SDK_INT > 9)
-        {
+        if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
         // Note: we need to request the permissions
         int requestCode = 5; // unique code for the permission request
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, INTERNET}, requestCode);
+
+        hear = findViewById(R.id.button);
+        b1 = (Button) findViewById(R.id.speak);
+
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
     }
 
     public void onSpeechButtonClicked(View v) {
-        TextView txt = (TextView) this.findViewById(R.id.hello); // 'hello' is the ID of your text view
+
+
+        hear.setText("Listening");
+
+        // TextView txt = (TextView) this.findViewById(R.id.hello); // 'hello' is the ID of your text view
 
         try {
             SpeechConfig config = SpeechConfig.fromSubscription(speechSubscriptionKey, serviceRegion);
@@ -70,9 +83,9 @@ public class MainActivity extends AppCompatActivity {
             assert (result != null);
 
             if (result.getReason() == ResultReason.RecognizedSpeech) {
-                txt.setText(result.getText().toString());
+                recognized = true;
             } else {
-                txt.setText("Error recognizing. Did you update the subscription info?" + System.lineSeparator() + result.toString());
+                recognized = false;
             }
 
             reco.close();
@@ -80,43 +93,61 @@ public class MainActivity extends AppCompatActivity {
             Log.e("SpeechSDKDemo", "unexpected " + ex.getMessage());
             assert (false);
         }
+        hear.setText("Ready to play");
 
-
-        setContentView(R.layout.activity_main);
-        b1=(Button)findViewById(R.id.speak);
-
-
-
-        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status != TextToSpeech.ERROR) {
-                    t1.setLanguage(Locale.UK);
-                }
-            }
-        });
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String toSpeak = nonPrafanity();
-                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
-                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
-            }
-        });
     }
 
-    static  String[] toArray(String s) {
-        String[] words = s.split("\\W+");
+    public void onSpeakButtonClicked(View view) {
+        try {
+            if (recognized) {
+
+                String toSpeak = nonPrafanity();
+                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+
+            } else {
+                String toSpeak = "Cant hear bleep";
+                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+
+
+            }
+            hear.setText("Hear");
+        } catch (Exception e) {
+            String toSpeak = "Something went wrong, please try again";
+            Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+            t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    static String[] toArray(String s) {
+        String[] words = s.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() > 1 && words[i].charAt(1) == '*') {
+                words[i] = new String("bleep");
+            }
+            words[i] = new String(words[i].replaceAll("[^a-zA-Z ]", ""));
+
+        }
+        loge(words);
         return words;
     }
 
-    String nonPrafanity(){
+    String nonPrafanity() {
         RemoveProfanity rp = new RemoveProfanity();
-       String stringgg = rp.run(toArray(result.getText()));
-        Log.e("Result",stringgg);
+        String stringgg = rp.run(toArray(result.getText()));
+        Log.e("Result", stringgg);
+
         return stringgg;
     }
 
+    static void loge(String[] what) {
+        StringBuilder lol = new StringBuilder();
+        for (int i = 0; i < what.length; i++) {
+            lol.append(what[i]);
+            // result +=(what[i]);
+        }
+        Log.e("Array", lol.toString());
+    }
 
 }
--- app/src/main/java/dev/kindeep/mlh18/RemoveProfanity.java
+++ app/src/main/java/dev/kindeep/mlh18/RemoveProfanity.java
@@ -7,91 +7,83 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class RemoveProfanity {
-	JsonParser jp = new JsonParser();
-	JsonObject o;
-	JsonArray ja;
+    JsonParser jp = new JsonParser();
+    JsonObject o;
+    JsonArray ja;
 
-	public RemoveProfanity() {
-		// TODO Auto-generated constructor stub
+    public String run(String[] sentence) {
+        boolean[] offense = new boolean[sentence.length];
 
-		//String[] sentence = { "What", "fucking", "heck" };
-		//String x = run(sentence);
-		//System.out.println(x);
+        // creates array of booleans (corresponds to words in sentence).
+        for (int i = 0; i < sentence.length; i++) {
+            boolean result = isOffensive(sentence[i]);
+            offense[i] = result;
+            System.out.print(result + ", ");
+        }
+        System.out.println();
+        StringBuilder sb = new StringBuilder();
 
-	}
+        // builds sentence using censored.
+        for (int i = 0; i < sentence.length; i++) {
+            if (!offense[i]) {
+                sb.append(sentence[i]);
+            } else {
+                if (sentence[i].endsWith("ing"))
+                    sb.append("bleeping");
+                else
+                    sb.append("bleep");
+            }
 
-	public String run(String[] sentence) {
-		boolean[] offense = new boolean[sentence.length];
+            sb.append(" ");
+        }
+        String result = sb.toString();
 
-		// creates array of booleans (corresponds to words in sentence).
-		for (int i = 0; i < sentence.length; i++) {
-			boolean result = isOffensive(sentence[i]);
-			offense[i] = result;
-			System.out.print(result + ", ");
-		}
-		System.out.println();
-		StringBuilder sb = new StringBuilder();
+        return result;
+    }
 
-		// builds sentence using censored.
-		for (int i = 0; i < sentence.length; i++) {
-			if (!offense[i]) {
-				sb.append(sentence[i]);
-			} else {
-				if (sentence[i].endsWith("ing"))
-					sb.append("bleeping");
-				else 
-					sb.append("bleep");
-			}
+    /*
+     * checks if a word is offensive.
+     */
+    private boolean isOffensive(String word) {
+        try {
+            Log.e("TheWord", word);
+            // builds url.
+            StringBuilder sb = new StringBuilder();
+            sb.append(new String("https://www.dictionaryapi.com/api/v3/references/collegiate/json/"));
+            sb.append(word);
+            sb.append(new String("?key=ee7e0255-db39-4b20-895b-91b0138134a0"));
+            URL url = new URL(sb.toString());
 
-			sb.append(" ");
-		}
-		String result = sb.toString();
+            // gets results from api.
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
 
-		return result;
-	}
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            ja = jp.parse(content.toString()).getAsJsonArray();
+            o = (JsonObject) ja.get(0);
 
-	/*
-	 * checks if a word is offensive.
-	 */
-	private boolean isOffensive(String word) {
-		try {
-			Log.e("TheWord", word);
-			// builds url.
-			StringBuilder sb = new StringBuilder();
-			sb.append(new String("https://www.dictionaryapi.com/api/v3/references/collegiate/json/"));
-			sb.append(word);
-			sb.append(new String("?key=ee7e0255-db39-4b20-895b-91b0138134a0"));
-			URL url = new URL(sb.toString());
+            JsonElement result = o.get("meta").getAsJsonObject().get("offensive");
+            in.close();
+            return result.getAsBoolean();
 
-			// gets results from api.
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
-			con.setRequestProperty("Content-Type", "application/json");
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer content = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-				content.append(inputLine);
-			}
-			ja = jp.parse(content.toString()).getAsJsonArray();
-			o = (JsonObject) ja.get(0);
-
-			JsonElement result = o.get("meta").getAsJsonObject().get("offensive");
-			in.close();
-			return result.getAsBoolean();
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}
-	}
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }
